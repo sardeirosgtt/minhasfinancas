e4c18c3b7d2029bd7673b6e08b224cd04f9068d0
Sgt Sardeiro <sardeiro.augusto@CDS.LOCAL>
2022-11-03 16:37:47 -0300

    aula 56

--- src/main/java/com/sardeiro/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/sardeiro/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,10 +1,42 @@
 package com.sardeiro.minhasfinancas.api.resource;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sardeiro.minhasfinancas.api.dto.UsuarioDTO;
+import com.sardeiro.minhasfinancas.exception.RegraNegocioException;
+import com.sardeiro.minhasfinancas.model.entity.Usuario;
+import com.sardeiro.minhasfinancas.service.UsuarioService;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
+	
+	private UsuarioService service;
+	
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+	
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+				.nome(dto.getNome())
+				.email(dto.getEmail())
+				.senha(dto.getSenha()).build();
+		
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo,HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+	}
 	
 }
